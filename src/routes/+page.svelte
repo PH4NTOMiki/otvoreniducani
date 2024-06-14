07ce1e531fe98778cf43be55c135e09d3efa83fa
Mihael <mihaelmiki123@live.com>
2024-06-14 20:33:30 +0200

    init

---
+++ src/routes/+page.svelte
@@ -0,0 +1,27 @@
+<script>
+import { onMount } from 'svelte';
+
+/** @type {import('./$types').PageData} */
+export let data;
+
+onMount(() => {
+    navigator.geolocation.getCurrentPosition(async ({coords}) => {
+        console.log(coords);
+        //const a = await (await fetch('/api/closest-store', {method:'POST', body: JSON.stringify(coords)})).json();
+        //console.log(a);
+    }, console.error);
+});
+</script>
+
+<div class="overflow-x-auto">
+    <table class="table mx-auto lg:w-4/5">
+        {#each data.stores as store}
+            <tr>
+                <td>{store.id}</td>
+                <td>{store.title}</td>
+                <td>{store.address}</td>
+
+            </tr>
+    {/each}
+    </table>
+</div>
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+import daisyui from 'daisyui';
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  purge: ['./src/**/*.svelte', './src/**/*.css'],
+  content: ['./src/**/*.{html,js,svelte,ts}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [daisyui],
+}
+
