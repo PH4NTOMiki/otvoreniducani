c0e195b00654ddbbeaf939b1b312399d6d31c3b5
Mihael <mihaelmiki123@live.com>
2024-06-15 14:57:21 +0200

    .

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,18 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import { resolve } from 'path';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	resolve: {
+		alias: {
+			$components: resolve('./src/components'),
+			$src: resolve('./src'),
+			$params: resolve('./src/params')
+		}
+	},
+	server: {
+		host: true,
+		port: 3000
+	}
 });
