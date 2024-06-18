50eb82e67b060a656ecb0141fab786275da5b588
Mihael <mihaelmiki123@live.com>
2024-06-18 17:49:37 +0200

    .

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,6 +9,7 @@ let { stores } = data;
 
 onMount(() => {
     var map = L.map('map').setView([45.8081751, 15.9841489], 12);
+    window.map = map;
     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
     stores.forEach(store => {
         L.marker([store.coordinate_x, store.coordinate_y]).addTo(map).bindPopup(`${store.title}<br>${store.address}<br>${store.town}`);
@@ -16,7 +17,8 @@ onMount(() => {
     
     navigator.geolocation.getCurrentPosition(async ({coords}) => {
         console.log(coords);
-        L.marker([coords.latitude,coords.longitude], {icon: L.icon({iconUrl:'https://cdn-icons-png.freepik.com/512/5683/5683273.png',iconSize:[38,48]})}).addTo(map);
+        L.marker([coords.latitude,coords.longitude], {icon: L.divIcon({className:'', html: '<svg height="50" width="50" xmlns="http://www.w3.org/2000/svg"><circle r="14" cx="25" cy="25" fill="#7272da" stroke="#52529f" stroke-width="7" /></svg>'})}).addTo(map);
+        map.panTo([coords.latitude,coords.longitude]);
         //alert(JSON.stringify({latitude:coords.latitude,longitude:coords.longitude,accuracy:coords.accuracy}));
         //const a = await (await fetch('/api/closest-store', {method:'POST', body: JSON.stringify(coords)})).json();
         //console.log(a);
