9998331526a35c76d952983df54901dc5957bc7d
Mihael <mihaelmiki123@live.com>
2024-08-16 19:27:04 +0200

    fix some auth issues

--- src/hooks.server.js
+++ src/hooks.server.js
@@ -2,9 +2,11 @@ import jwt from 'jsonwebtoken';
 import { SECRET_KEY } from '$env/static/private';
 import { clearFetchCache } from '$lib/db';
 import { findUserByUsername } from '$lib/user-server';
+import { user } from '$lib/auth';
 
 /** @type {import('@sveltejs/kit').Handle} */
 export async function handle({ event, resolve }) {
+    user.set(null);
     clearFetchCache();
     const token = event.cookies.get('token');
     const refreshToken = event.cookies.get('refreshToken');
@@ -14,6 +16,7 @@ export async function handle({ event, resolve }) {
             const decoded = jwt.verify(token, SECRET_KEY);
             // @ts-ignore
             event.locals.user = decoded;
+            user.set(event.locals.user);
         } catch (err) {
             // Token is invalid, clear it
             //event.cookies.delete('token', { path: '/' });
@@ -22,9 +25,10 @@ export async function handle({ event, resolve }) {
         try {
             const decoded = jwt.verify(refreshToken, SECRET_KEY);
             // @ts-ignore
-            const user = await findUserByUsername(decoded.username);
-            const newToken = jwt.sign(user, SECRET_KEY, { expiresIn: '15m' });
+            const tempUser = await findUserByUsername(decoded.username);
+            const newToken = jwt.sign(tempUser, SECRET_KEY, { expiresIn: '15m' });
             event.locals.user = JSON.parse(Buffer.from(newToken.split('.')[1], 'base64').toString());
+            user.set(event.locals.user);
     
             event.cookies.set('token', newToken, {
                 httpOnly: true,
