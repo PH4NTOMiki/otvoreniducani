a3c29c5db9371b69803d89f1a432d1d8e8230a48
Mihael <mihaelmiki123@live.com>
2024-08-16 20:44:55 +0200

    .

--- src/routes/upravljanje/+page.svelte
+++ src/routes/upravljanje/+page.svelte
@@ -24,7 +24,7 @@
                 <tr class="bg-base-200">
                     <th class="hidden"></th>
                     <th class="text-lg font-semibold">Dućan</th>
-                    <th class="text-lg font-semibold">Adresa</th>
+                    <th class="text-lg font-semibold">Lokacija</th>
                 </tr>
             </thead>
             <tbody>
@@ -53,7 +53,7 @@
                             <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mr-2 text-secondary" viewBox="0 0 20 20" fill="currentColor">
                                 <path fill-rule="evenodd" d="M5.05 4.05a7 7 0 119.9 9.9L10 18.9l-4.95-4.95a7 7 0 010-9.9zM10 11a2 2 0 100-4 2 2 0 000 4z" clip-rule="evenodd" />
                             </svg>
-                            {store.address}
+                            {store.address}, {store.town}
                         </td>
                     </tr>
                 {:else}
