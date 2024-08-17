cfa67d0ec5837ae05c379d9f42252db688c1c532
Mihael <mihaelmiki123@live.com>
2024-08-17 17:36:35 +0200

    .

--- src/app.d.ts
+++ src/app.d.ts
@@ -19,6 +19,27 @@ declare global {
 		// interface PageData {}
 		// interface PageState {}
 		// interface Platform {}
+		interface Store {
+			id: number;
+			default_start: string | null[];
+			default_end: string | null[];
+			distance: number?;
+			current_start: string?;
+			current_end: string?;
+			created_at: string;
+			address: string;
+			town: string;
+			coordinate_x: number;
+			coordinate_y: number;
+			title: string;
+			store_days: {
+				id: number;
+				store_id: number;
+				date: string;
+				start: string | null;
+				end: string | null
+			}[];
+		}
 	}
 	L = import('@types/leaflet')
 }
